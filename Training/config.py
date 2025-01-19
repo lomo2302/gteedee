53b5814fbb32f329acc28233a26fc4c24b3f8498
lomo2302 <456easy123@gmail.com>
2025-01-19 07:28:23 -0600

    Add files via upload

---
+++ Training/config.py
@@ -0,0 +1,69 @@
+# GAME SETTINGS
+
+# Screen width in pixels
+Option_screen_width = 384
+# Screen height in pixels
+Option_screen_height = 216
+
+# Field of view in x direction (game settings) affects the mouse left and right
+Option_fov_x = 60
+# Field of view in y direction (game settings) affects the mouse up and down
+Option_fov_y = 60
+
+# Mouse DPI (dots per inch) - higher DPI means more sensitive virtual mouse
+Option_mouse_dpi = 250
+# Mouse sensitivity means how much the mouse moves in relation to the virtual mouse DPI
+Option_mouse_sensitivity = 0.75
+
+# Data settings
+# Delete previous generated data True or False (use capital letter on first letter of True or False) default is True
+Option_delete_prev_data = True
+# Generation settings True or False (use capital letter on first letter of True or False) default is True
+Option_Generation = True
+# Time in seconds to generate data
+Option_gen_time = 600
+# Visualise generation True or False (use capital letter on first letter of True or False) default is True
+Option_gen_visualise = True
+# Visualise draw line True or False (use capital letter on first letter of True or False) default is True
+Option_gen_visualise_draw_line = True
+
+# Train model settings True or False (use capital letter on first letter of True or False) default is True
+Option_train = True
+# Number of epochs how many rounds to train the model
+Option_train_epochs = 200
+# Batch size - how many samples to process at once (higher is faster but requires more memory)
+Option_train_batch_size = 12500
+# Save every N epoch - how many epochs to save the model periodically
+Option_save_every_N_epoch = 1
+# Learning rate The learning rate controls how quickly the model is adapted to the problem.
+# Smaller values require more training epochs, but larger values may not train the model effectively.
+Option_learning_rate = 0.00001
+
+# Speed - 1 is max speed, 0.5 is half speed, -1 is reverse max speed and -0.5 is reverse half speed default is 1
+Option_gen_speed_x = [-1, 1]
+Option_gen_speed_y = [-1, 1]
+
+# Game settings - random values
+# Random screen resolution True or False (use capital letter on first letter of True or False) default is False
+Option_random_screen_resolution = False
+# Random screen resolution width (random screen size in pixels left and right)
+Option_random_screen_resolution_width = [150, 600]
+# Random screen resolution height (random screen size in pixels up and down)
+Option_random_screen_resolution_height = [150, 600]
+# Random field of view True or False (use capital letter on first letter of True or False) default is False
+Option_random_fov = False
+# Random field of view in x direction (random field of view in pixels left and right)
+Option_random_fov_x = [45, 90]
+# Random field of view in y direction (random field of view in pixels up and down)
+Option_random_fov_y = [45, 90]
+# Random mouse DPI True or False (use capital letter on first letter of True or False) default is False
+Option_random_mouse_dpi = False
+# Random mouse DPI min and max (random mouse DPI in pixels)
+Option_random_mouse_dpi_min_max = [100, 1000]
+# Random mouse sensitivity True or False (use capital letter on first letter of True or False) default is False
+Option_random_mouse_sensitivity = False
+# Random mouse sensitivity min and max (random mouse sensitivity in pixels)
+Option_random_mouse_sensitivity_min_max = [0.3, 1.5]
+
+# Testing model settings True or False (use capital letter on first letter of True or False)
+Option_test_model = True
